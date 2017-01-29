f356408092039b3702b0f78c065a15e5ac655e52
marcAndreTremblay <marct549@gmail.com>
2017-01-29 13:00:32 -0500

    Removed some dependencies

---
+++ log430-lab1/src/main/java/edu/gordon/banking/ATMInformation.java
@@ -0,0 +1,16 @@
+package edu.gordon.banking;
+
+
+
+public class ATMInformation {
+	
+	public String Place;
+	public int Id;
+	public String BankName;
+	public ATMInformation(int id, String place, String bankName){
+		Place = place;
+		Id = 	id;
+		BankName = bankName;
+	}
+	
+}
